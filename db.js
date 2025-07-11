2bc07e91ec46cbbab88eefd7eb88ff0d454ea884
VenkataSiriPriya <2300030401@kluniversity.in>
2025-07-11 23:08:22 +0530

    Backend created

--- db.js
+++ db.js
@@ -1,6 +1,11 @@
-const { Pool } = require('pg');
 require('dotenv').config();
+const { Pool } = require('pg');
 
-const pool = new Pool(); // uses env variables
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // required for Render
+  },
+});
 
 module.exports = pool;
